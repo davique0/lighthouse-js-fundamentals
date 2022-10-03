447f8085905d6cb013a2e13d04bc5de58934f703
davique0 <davique@gmail.com>
2022-10-03 08:28:13 -0400

    assignement loopy light house 2, with arrays as arguments

---
+++ LoopyLighthouse-2.js
@@ -0,0 +1,21 @@
+/*Wrap the code you've written in a function called loopyLighthouse(range, multiples, words) that you call with 3 parameters.
+
+range should be an array of 2 numbers representing the start and end values for the loop.
+multiples should be an array of 2 numbers representing the multiples you want to replace with words.
+words should be an array of 2 strings representing the words that will replace the multiples.*/
+
+function loopyLighthouse(range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else if (i % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+loopyLighthouse([5, 50], [3, 4], ['Mario', 'David']);
